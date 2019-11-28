909bbc3d23c660ec9244dcc3bbf08edcb7fb47ed
ypenghui6 <ypenghui6@163.com>
2019-11-28 15:30:54 +0800

    update the scripts

--- create_keys/README.md
+++ create_keys/README.md
@@ -1,9 +1,12 @@
 # create_account_and_asset
 
 # info
-the bps.txt are the result.  
+the bps_keys.txt record the all keys.   
+the bps_pub_keys.txt record the public keys.   
+the bps_wif_priv_keys.txt record the awif_priv keys.   
+the bps_brain_priv_keys.txt record the brain_priv keys.   
 
 # run
-first update the connection information of the connect_cli_wallet.sh, run it: ./connect_cli_wallet.sh a  
-then you need change the account in the create_keys.py.   
-last run the script.  
+first update the chain-id and ws of the connect_cli_wallet.sh   
+then run it: ./connect_cli_wallet.sh a  
+last run it: python3 create_keys.py   
--- create_keys/create_keys.py
+++ create_keys/create_keys.py
@@ -6,8 +6,12 @@ import os
 
 cli_wallet_url = "http://0.0.0.0:8066"
 headers = {"content-type": "application/json"}
-newfile = "bps.txt"
-keys_counts = 12
+bps_keys_file = "bps_keys.txt"
+bps_pub_keys_file = "bps_pub_keys.txt"
+bps_wif_priv_keys_file = "bps_wif_priv_keys.txt"
+bps_brain_priv_keys_file = "bps_brain_priv_keys.txt"
+
+keys_counts = 13
 
 def suggest_brain_key():
     try:
@@ -25,7 +29,7 @@ def suggest_brain_key():
         print(repr(e))
 
 
-def save_account(str):
+def save_account(str, newfile):
     if not os.path.exists(newfile):
         f = open(newfile,'w')
         print(newfile)
@@ -43,8 +47,18 @@ def create_keys():
     while count < keys_counts:
         count = count + 1
         account_key = suggest_brain_key()
-        tmp = json.dumps(account_key)
-        save_account(tmp)
-            
+
+        tmp_key = json.dumps(account_key)
+        save_account(tmp_key, bps_keys_file)
+
+        tmp_pub_key = json.dumps(account_key['pub_key'])
+        save_account(tmp_pub_key, bps_pub_keys_file)
+
+        tmp_priv_key = json.dumps({"wif_priv_key":account_key['wif_priv_key'], "pub_key":account_key['pub_key']})
+        save_account(tmp_priv_key, bps_wif_priv_keys_file)
+
+        tmp_brain_priv_key = json.dumps({"brain_priv_key":account_key['brain_priv_key'], "pub_key":account_key['pub_key']})
+        save_account(tmp_brain_priv_key, bps_brain_priv_keys_file)            
+
 
 create_keys()
